e18bd9cac6ffe5e35419126de992143702f81ff3
= <=>
2022-04-02 14:12:44 +0100

    random menu setup stuff relay

---
+++ Assets/Scripts/Server/Portals/TestRelayPortal.cs
@@ -0,0 +1,70 @@
+using shriller.Core.Singletons;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestRelayPortal : Singleton<TestRelayPortal>
+{
+    [SerializeField]
+    private Button startHostButton;
+
+    [SerializeField]
+    private Button startClientButton;
+
+    [SerializeField]
+    private TMP_InputField joinCodeInput;
+
+    private bool hasServerStarted;
+
+    private void Awake()
+    {
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+    }
+
+    void Start()
+    {
+        // START HOST
+        startHostButton?.onClick.AddListener(async () =>
+        {
+
+            Debug.Log("cunt");
+            if (RelayManager.Instance.IsRelayEnabled)
+                await RelayManager.Instance.SetupRelay();
+
+            if (NetworkManager.Singleton.StartHost())
+                Debug.Log("test");
+            else
+                Debug.Log("test");
+        });
+
+
+        startClientButton?.onClick.AddListener(async () =>
+        {
+
+            if (RelayManager.Instance.IsRelayEnabled && !string.IsNullOrEmpty(joinCodeInput.text))
+                await RelayManager.Instance.JoinRelay(joinCodeInput.text);
+
+            if (NetworkManager.Singleton.StartClient())
+                Debug.Log("test");
+            else
+                Debug.Log("test");
+        });
+
+        // STATUS TYPE CALLBACKS
+        NetworkManager.Singleton.OnClientConnectedCallback += (id) =>
+        {
+
+        };
+
+        NetworkManager.Singleton.OnServerStarted += () =>
+        {
+            hasServerStarted = true;
+        };
+
+    }
+}
