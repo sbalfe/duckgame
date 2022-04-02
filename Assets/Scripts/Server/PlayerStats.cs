7c9718e913062a99443726d2cd1bcf8f8f64062d
Sam Brigs <spbriggs01@gmail.com>
2022-04-02 13:46:31 +0100

    Duck stuff

--- Assets/Scripts/Server/PlayerStats.cs
+++ Assets/Scripts/Server/PlayerStats.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
+<<<<<<< Updated upstream:Assets/Scripts/Server/PlayerStats.cs
 // TODO:Change to network behaviour
 public class PlayerStats : NetworkBehaviour
 {
@@ -11,6 +12,17 @@ public class PlayerStats : NetworkBehaviour
     [SerializeField] private int health = 100;
 
     public override void OnNetworkSpawn()
+=======
+public class PlayerStats : NetworkBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+    
+    private NetworkVariable<int> health = new(100);
+    
+
+    // Start is called before the first frame update
+    void Start()
+>>>>>>> Stashed changes:Assets/Scripts/PlayerStats.cs
     {
         if (IsOwner)
         {
@@ -31,6 +43,17 @@ public class PlayerStats : NetworkBehaviour
         if (Input.GetAxis("Vertical") != 0)
         {
             transform.Translate(new Vector3(0, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed, 0));
+        }
+    }
+
+    [ServerRpc]
+    public void TakeDamageServerRpc(int damage)
+    {
+        health.Value -= damage;
+        if (health.Value <= 0)
+        {
+            // Die
+            GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
 }
